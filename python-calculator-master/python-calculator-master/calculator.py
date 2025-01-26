e71814a1aaa94a8cefe8836a7d19460f5a58ebc9
Siratheenext <siratheenext@gmail.com>
2025-01-26 14:01:08 +0700

    Add files via upload

---
+++ python-calculator-master/python-calculator-master/calculator.py
@@ -0,0 +1,34 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        result = 0
+        for i in range(b):
+            result = self.add(result, a)
+        return result
+
+    def divide(self, a, b):
+        result = 0
+        while a > b:
+            a = self.subtract(a, b)
+            result += 1
+        return result + 1
+    
+    def modulo(self, a, b):
+        while b <= a:
+            a = a-b
+        return a
+
+# Example usage:
+if __name__ == "__main__":
+    calc = Calculator()
+    print("This is a simple calculator class!")
+    print("Example: addition: ", calc.add(1, 2))
+    print("Example: subtraction: ", calc.subtract(4, 2))
+    print("Example: multiplication: ", calc.multiply(2, 3))
+    print("Example: division: ", calc.divide(10, 2))
+    print("Example: modulo: ", calc.modulo(10, 3))
